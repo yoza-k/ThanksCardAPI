3df44a988d69b4d90053a48ba8eb30b2bbb651d2
shinoburc <dandydot@gmail.com>
2019-05-16 16:35:03 +0900

    Add Rank Controller and Model.

---
+++ Models/Rank.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ThanksCardAPI.Models
+{
+    public class Rank
+    {
+        public string Name { get; set; }
+        public int Count { get; set; }
+    }
+}
