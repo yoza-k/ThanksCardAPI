2e6a2fc87c8b21ba4987fa8917aa0ef1429e1f6b
shinoburc <dandydot@gmail.com>
2019-05-24 18:18:38 +0900

    add department hierarchie.

--- Migrations/20190524003102_AddParentAndChildrenColumn.Designer.cs
+++ Migrations/20190524003102_AddParentAndChildrenColumn.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using ThanksCardAPI.Models;
@@ -9,9 +10,10 @@ using ThanksCardAPI.Models;
 namespace ThanksCardAPI.Migrations
 {
     [DbContext(typeof(ApplicationContext))]
-    partial class ApplicationContextModelSnapshot : ModelSnapshot
+    [Migration("20190524003102_AddParentAndChildrenColumn")]
+    partial class AddParentAndChildrenColumn
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
