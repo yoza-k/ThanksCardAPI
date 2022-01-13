31b4e2db99a025f54d8d6c622b0365700b5e8383
Toshi7 <ttoshi0719@gmail.com>
2022-01-13 18:35:45 +0900

    JSONシリアル化で循環参照を無視

--- Program.cs
+++ Program.cs
@@ -2,15 +2,20 @@ using Microsoft.EntityFrameworkCore;
 using ThanksCardAPI.Models;
 using Microsoft.Extensions.Configuration;
 using Microsoft.OpenApi.Models;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
-builder.Services.AddControllers();
+// JSONシリアル化で循環参照を無視
+builder.Services.AddControllers().AddJsonOptions(option =>
+                option.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
 builder.Services.AddDbContext<ApplicationContext>(opt =>
      opt.UseNpgsql("Host=localhost; Database=webapp1; Username=postgres; Password=postgres"));
+
 builder.Services.AddControllers();
+
 // API を一覧表示する Swagger の設定
 builder.Services.AddSwaggerGen(options =>
 {
