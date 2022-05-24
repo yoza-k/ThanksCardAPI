7cca4ee367e91c2a6d38610a96794c25120e6a67
keisuke_yoza <yoza_k@occ.co.jp>
2022-05-24 10:14:53 +0900

    ad First commit

--- Program.cs
+++ Program.cs
@@ -12,7 +12,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers().AddJsonOptions(option =>
                 option.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
 builder.Services.AddDbContext<ApplicationContext>(opt =>
-     opt.UseNpgsql("Host=localhost; Database=webapp1; Username=postgres; Password=postgres"));
+     opt.UseNpgsql("Host=localhost; Database=thankscard; Username=postgres; Password=admin"));
 // DateTime å^ Ç UTC Ç≈ ÉfÅ[É^Ç PostgreSQL Ç…ìoò^
 AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
 
